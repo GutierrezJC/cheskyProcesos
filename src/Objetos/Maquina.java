03746dd6f151a487520934536b60c848af5bfdbb
LuisHCode <yolothelife95@gmail.com>
2024-11-24 17:35:21 -0600

    Creacion de asignacion y liberacion de recursos
    Se creo la clase maquina para asignar y liberarle recursos, ademas de la creacion de la clase recurso y la modificacion de procesos para que tenga una lista de recursos, en los metodos de round robin y demas, se añadio la liberacion y asignacion de recursos de la maquina, ademas de un metodo que libera los procesos que sobrepasan los recursos de la maquina, se debe revisar bien el netodo ese para verifivar que no de error en los procesos

--- src/Objetos/Maquina.java
+++ src/Objetos/Maquina.java
@@ -1,16 +1,252 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Objetos;
+
+import java.util.ArrayList;
 
 /**
  *
- * @author jeank
+ * @author luisi
  */
 public class Maquina {
-    
-    
-   
-    
+
+    private ArrayList<Recurso> listaRecursoMaquina;
+    private ArrayList<Recurso> listaRecursoMaquinaDisponibles;
+    private int unidadesMemoriaMaquina;
+    private int unidadesMemoriaDisponible;
+    private int unidadesMemoriaUtilizada;
+
+    private int unidadesCPUMaquina;
+    private int unidadesCPUDisponible;
+    private int unidadesCPUUtilizadas;
+    private String nombre;
+    private String ubicacion;
+    private int tiempoInicio;   //Tiempo en que la maquina inicia.
+
+    //Constructores
+    public Maquina() {
+        this.unidadesMemoriaMaquina = 0;
+        this.unidadesMemoriaUtilizada = 0;
+        this.unidadesCPUMaquina = 0;
+        this.unidadesCPUUtilizadas = 0;
+        this.nombre = "Maquina";
+        this.ubicacion = "PCI";
+        this.tiempoInicio = 0;
+        this.unidadesMemoriaDisponible = 0;
+        this.unidadesCPUDisponible = 0;
+        inicializarMaquinasRecursos();
+    }
+
+    public Maquina(int unidadesMemoriaMaquina, String nombre,
+            String ubicacion, int tiempoInicio, ArrayList<Recurso> listaRecursos)//crear unidadesCPUMaquina
+    {
+        this.unidadesMemoriaMaquina = unidadesMemoriaMaquina;
+        this.unidadesMemoriaUtilizada = 0;
+        this.unidadesCPUMaquina = unidadesCPUMaquina;
+        this.unidadesCPUUtilizadas = 0;
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.tiempoInicio = tiempoInicio;
+        this.unidadesMemoriaDisponible = unidadesMemoriaMaquina;
+        this.unidadesCPUDisponible = unidadesCPUMaquina;
+        this.listaRecursoMaquina = listaRecursos;
+        this.listaRecursoMaquinaDisponibles = listaRecursos;
+    }
+
+    public Maquina(ArrayList<Recurso> listaNueva, int unidadesMemoriaMaquina,
+            String nombre, String ubicacion, int tiempoInicio, ArrayList<Recurso> listaRecursos) {
+        this.listaRecursoMaquina = listaNueva;
+        this.unidadesMemoriaMaquina = unidadesMemoriaMaquina;
+        this.unidadesMemoriaUtilizada = 0;
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.tiempoInicio = tiempoInicio;
+        this.unidadesMemoriaDisponible = unidadesMemoriaMaquina;
+        this.listaRecursoMaquina = listaRecursos;
+        this.listaRecursoMaquinaDisponibles = listaRecursos;
+
+    }
+
+    //Metodos set y get
+    public void setUnidadesMemoriaMaquina(int unidades) {
+        this.unidadesMemoriaMaquina = unidades;
+    }
+
+    public void setUnidadesCPUMaquina(int unidades) {
+        this.unidadesCPUMaquina = unidades;
+    }
+
+    public void setNombre(String nom) {
+        this.nombre = nom;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public void setTiempoInicio(int tiempo) {
+        this.tiempoInicio = tiempo;
+    }
+
+    public int getUnidadesMemoriaMaquina() {
+        return this.unidadesMemoriaMaquina;
+    }
+
+    public ArrayList<Recurso> getListaRecursoMaquina() {
+        return listaRecursoMaquina;
+    }
+
+    public void setListaRecursoMaquina(ArrayList<Recurso> listaRecursoMaquina) {
+        this.listaRecursoMaquina = listaRecursoMaquina;
+    }
+
+    public ArrayList<Recurso> getListaRecursoMaquinaDisponibles() {
+        return listaRecursoMaquinaDisponibles;
+    }
+
+    public void setListaRecursoMaquinaDisponibles(ArrayList<Recurso> listaRecursoMaquinaDisponibles) {
+        this.listaRecursoMaquinaDisponibles = listaRecursoMaquinaDisponibles;
+    }
+
+    public int getUnidadesCPUDisponible() {
+        return unidadesCPUDisponible;
+    }
+
+    public void setUnidadesCPUDisponible(int unidadesCPUDisponible) {
+        this.unidadesCPUDisponible = unidadesCPUDisponible;
+    }
+
+    public int getUnidadesCPUUtilizadas() {
+        return unidadesCPUUtilizadas;
+    }
+
+    public void setUnidadesCPUUtilizadas(int unidadesCPUUtilizadas) {
+        this.unidadesCPUUtilizadas = unidadesCPUUtilizadas;
+    }
+
+    public int getUnidadesMemoriaUtilizada() {
+        return this.unidadesMemoriaUtilizada;
+    }
+
+    public int getUnidadesMemoriaDisponible() {
+        return this.unidadesMemoriaDisponible;
+    }
+
+    public int getUnidadesCPUMaquina() {
+        return this.unidadesCPUMaquina;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getUbicacion() {
+        return this.ubicacion;
+    }
+
+    public int getTiempoInicio() {
+        return this.tiempoInicio;
+    }
+
+    public void liberarMemoria(int cantidadLiberar) {
+        if (cantidadLiberar > unidadesMemoriaUtilizada) {
+            return;
+        }
+        this.unidadesMemoriaDisponible += cantidadLiberar;
+        this.unidadesMemoriaUtilizada -= cantidadLiberar;
+    }
+
+    public void asignarMemoria(int cantidadAsignar) {
+        if (cantidadAsignar > unidadesMemoriaDisponible) {
+            return;
+        }
+        this.unidadesMemoriaDisponible -= cantidadAsignar;
+        this.unidadesMemoriaUtilizada += cantidadAsignar;
+    }
+
+    public void inicializarMaquinasRecursos() {
+        listaRecursoMaquina = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            listaRecursoMaquina.add(new Recurso(i, "Recurso_" + i, 100 - (i * 10)));
+        }
+        listaRecursoMaquinaDisponibles = listaRecursoMaquina;
+    }
+
+    public String listaRecursosToString() {
+        String mensaje = "";
+        for (int i = 0; i < listaRecursoMaquina.size(); i++) {
+            mensaje += "Recurso " + (i + 1) + ": " + listaRecursoMaquina.get(i).getUnidades() + "\n";
+        }
+        return mensaje;
+    }
+
+    public String toString() {
+        String sal = "Maquina..............." + getNombre();
+        sal += "\nUbicacion..........." + getUbicacion();
+        sal += "\nTiempo de Inicio...." + getTiempoInicio();
+        sal += "\nMemoria Disponible ." + getUnidadesMemoriaMaquina();
+        sal += "\nMemoria Usada......." + getUnidadesMemoriaUtilizada();
+        sal += "\nLista de Recursos:.." + "\n";
+        sal += listaRecursosToString();
+
+        return sal;
+    }
+
+    public String listarMaquinaDetalle() {
+        String sal = "" + getNombre();
+        sal += "\t\t" + getUbicacion();
+        sal += "\t" + getTiempoInicio();
+        sal += "\t" + unidadesMemoriaMaquina;
+        sal += "\t" + unidadesMemoriaDisponible;
+        sal += "\t" + unidadesMemoriaUtilizada;
+        sal += "\nLista de Recursos:\n";
+        sal += listaRecursosToString();
+
+        return sal;
+    }
+
+    public boolean recursosSobrepasaMaquina(ArrayList<Recurso> recursos) {
+        for (int i = 0; i < recursos.size(); i++) {
+            if (recursos.get(i).getUnidades() > listaRecursoMaquina.get(i).getUnidades()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // ASIGNAR RECURSOS A LA MAQUINA 
+    public void asignarRecurso(Recurso nuevo) {
+        listaRecursoMaquinaDisponibles.get(nuevo.getNrecurso()).asignarRecurso(nuevo.getUnidades());
+    }
+
+    public void liberarRecurso(Recurso nuevo) {
+        listaRecursoMaquinaDisponibles.set(nuevo.getNrecurso(), listaRecursoMaquina.get(nuevo.getNrecurso()));
+    }
+
+    public void asignarRecurso(ArrayList<Recurso> recursos) {
+        for (int i = 0; i < listaRecursoMaquina.size(); i++) {
+            asignarRecurso(recursos.get(i));
+        }
+    }
+
+    public void liberarRecurso(ArrayList<Recurso> recursos) {
+        for (int i = 0; i < listaRecursoMaquina.size(); i++) {
+            liberarRecurso(recursos.get(i));
+        }
+    }
+
+    public void asignarCPU(int cantidadAsignar) {
+        if (cantidadAsignar > unidadesCPUDisponible) {
+            return;
+        }
+        this.unidadesCPUDisponible -= cantidadAsignar;
+        this.unidadesCPUUtilizadas += cantidadAsignar;
+    }
+
+    public void liberarCPU(int cantidadLiberar) {
+        if (cantidadLiberar > unidadesCPUUtilizadas) {
+            return;
+        }
+        this.unidadesCPUDisponible += cantidadLiberar;
+        this.unidadesCPUUtilizadas -= cantidadLiberar;
+    }
+
 }
--- src/algoritmo/Prioridad.java
+++ src/algoritmo/Prioridad.java
@@ -6,9 +6,11 @@ import AlgoritmosRemplazo.OPT;
 import AlgortitmosSegmentacion.MejorAjuste;
 import AlgortitmosSegmentacion.PeorAjuste;
 import AlgortitmosSegmentacion.PrimerAjuste;
+import Objetos.Maquina;
 import Objetos.Pagina;
 import interfaz.VentanaPrincipalPrincipal;
 import Objetos.Proceso;
+import Objetos.Recurso;
 
 import java.util.Collections;
 import java.util.List;
@@ -25,125 +27,144 @@ import static utilidades.Calculos.dividirEnPartes;
 import static utilidades.Calculos.obtenerPuntosInicio;
 
 public class Prioridad {
-
+    
     private int tiempoActual = 0;
     private VentanaPrincipalPrincipal ventana;
     public boolean paginacionXsegmentacion;
     public int idalgoritmosRemplazo;
-
+    private Maquina maquina = new Maquina();
+    
     public Prioridad(VentanaPrincipalPrincipal ventana) {
         this.ventana = ventana;
     }
-
+    
     public void ejecutar(List<Proceso> procesos) {
         new ejecutarAPrioridad(procesos).execute();
     }
-
+    
+    public List<Proceso> liberarProcesosSobrepasoRecursos(List<Proceso> procesosOrdenados) {
+        for (int i = 0; i < procesosOrdenados.size(); i++) {
+            if (maquina.recursosSobrepasaMaquina(procesosOrdenados.get(i).getListaRecursos())) {
+                procesosOrdenados.remove(i);
+            }
+        }
+        return procesosOrdenados;
+    }
+    
+    public void asignarRecursos(ArrayList<Recurso> recursos) {
+        maquina.asignarRecurso(recursos);
+    }
+    
+    public void liberarRecursos(ArrayList<Recurso> recursos) {
+        maquina.liberarRecurso(recursos);
+    }
+    
     private class ejecutarAPrioridad extends SwingWorker<Void, Proceso> {
-
+        
         private List<Proceso> procesosOrdenados;
-
+        
         public ejecutarAPrioridad(List<Proceso> procesos) {
             procesosOrdenados = new ArrayList<>(procesos);
             Collections.sort(procesosOrdenados, (p1, p2) -> Integer.compare(p1.getPrioridad(), p2.getPrioridad()));
         }
-
+        
         @Override
         protected Void doInBackground() throws Exception {
             ArrayList<Particion> particiones = new ArrayList<>();
-
+            
             for (Proceso proceso : procesosOrdenados) {
                 correrTimempo(proceso);
                 cronometrarProceso(proceso);
-
+                
                 FIFO fifo = new FIFO(proceso.getTablaPaginas().getMaxMarcos(), proceso.getTablaPaginas());
                 LRU lru = new LRU(5, proceso.getTablaPaginas());
                 ArrayList<Integer> futurasReferencias = obtenerFuturasReferencias(proceso);
                 OPT opt = new OPT(5, proceso.getTablaPaginas(), futurasReferencias);
                 int rafaga = proceso.getTiempoRafaga();
-
+                
                 ArrayList<Integer> puntosFinales = dividirEnPartes(proceso.getCantidadPaginas(), proceso.getTiempoRafaga());
                 ArrayList<Integer> puntosInicio = obtenerPuntosInicio(puntosFinales);
-
+                
                 proceso.setPuntosFinales(puntosFinales);
                 proceso.setPuntosInicio(puntosInicio);
-
+                
                 if (paginacionXsegmentacion & idalgoritmosRemplazo == 1) {
-
+                    asignarRecursos(proceso.getListaRecursos());
                     while (rafaga > 0) {
-
+                        
                         int puntoinicio = proceso.getPuntosInicio().get(0);
                         int puntofinal = proceso.getPuntosFinales().get(0);
-
+                        
                         rafaga = cpu(rafaga, proceso);
-
+                        
                         for (int i = puntoinicio; i <= puntofinal; i++) {
-
+                            
                             Pagina pagina = proceso.getTablaPaginas().getPaginas().get(i);
-
+                            
                             fifo.accederPagina(pagina, "C:\\memoriavirtual\\" + pagina.getNombreArchivo());
-
+                            
                         }
-
+                        
                         publish(proceso);
-
+                        
                         proceso.getPuntosInicio().remove(0);
                         proceso.getPuntosFinales().remove(0);
                     }
-
+                    liberarRecursos(proceso.getListaRecursos());
                 }
-
+                
                 if (paginacionXsegmentacion & idalgoritmosRemplazo == 2) {
-                  
-
+                    asignarRecursos(proceso.getListaRecursos());
                     while (rafaga > 0) {
-
+                        
                         int puntoinicio = proceso.getPuntosInicio().get(0);
                         int puntofinal = proceso.getPuntosFinales().get(0);
-
+                        
                         rafaga = cpu(rafaga, proceso);
-
+                        
                         for (int i = puntoinicio; i <= puntofinal; i++) {
-
+                            
                             Pagina pagina = proceso.getTablaPaginas().getPaginas().get(i);
-
+                            
                             lru.referenciarPagina(pagina, "C:\\memoriavirtual\\" + pagina.getNombreArchivo());
-
+                            
                         }
-
+                        
                         publish(proceso);
-
+                        
                         proceso.getPuntosInicio().remove(0);
                         proceso.getPuntosFinales().remove(0);
                     }
+                    liberarRecursos(proceso.getListaRecursos());
                 }
-
+                
                 if (paginacionXsegmentacion & idalgoritmosRemplazo == 3) {
-                   
-
-                     while (rafaga > 0) {
-
+                    asignarRecursos(proceso.getListaRecursos());
+                    while (rafaga > 0) {
+                        
                         int puntoinicio = proceso.getPuntosInicio().get(0);
                         int puntofinal = proceso.getPuntosFinales().get(0);
-
+                        
                         rafaga = cpu(rafaga, proceso);
-
+                        
                         for (int i = puntoinicio; i <= puntofinal; i++) {
-
+                            
                             Pagina pagina = proceso.getTablaPaginas().getPaginas().get(i);
-
-                          opt.referenciarPagina(pagina, "C:\\memoriavirtual\\" + pagina.getNombreArchivo());
-
+                            
+                            opt.referenciarPagina(pagina, "C:\\memoriavirtual\\" + pagina.getNombreArchivo());
+                            
                         }
-
+                        
                         publish(proceso);
-
+                        
                         proceso.getPuntosInicio().remove(0);
                         proceso.getPuntosFinales().remove(0);
                     }
+                    liberarRecursos(proceso.getListaRecursos());
                 }
-
+                
                 if (!paginacionXsegmentacion && idalgoritmosRemplazo == 1) { // Primer Ajuste
+                    asignarRecursos(proceso.getListaRecursos());
                     PrimerAjuste primerAjuste = new PrimerAjuste();
                     if (primerAjuste.asignar(proceso, particiones)) {
                         while (rafaga > 0) {
@@ -154,9 +175,11 @@ public class Prioridad {
                     } else {
                         System.out.println("No se pudo asignar el proceso con Primer Ajuste.");
                     }
+                    liberarRecursos(proceso.getListaRecursos());
                 }
-
+                
                 if (!paginacionXsegmentacion && idalgoritmosRemplazo == 2) { // Mejor Ajuste
+                    asignarRecursos(proceso.getListaRecursos());
                     MejorAjuste mejorAjuste = new MejorAjuste();
                     if (mejorAjuste.asignar(proceso, particiones)) {
                         while (rafaga > 0) {
@@ -167,9 +190,11 @@ public class Prioridad {
                     } else {
                         System.out.println("No se pudo asignar el proceso con Mejor Ajuste.");
                     }
+                    liberarRecursos(proceso.getListaRecursos());
                 }
-
+                
                 if (!paginacionXsegmentacion && idalgoritmosRemplazo == 3) { // Peor Ajuste
+                    asignarRecursos(proceso.getListaRecursos());
                     PeorAjuste peorAjuste = new PeorAjuste();
                     if (peorAjuste.asignar(proceso, particiones)) {
                         while (rafaga > 0) {
@@ -180,17 +205,17 @@ public class Prioridad {
                     } else {
                         System.out.println("No se pudo asignar el proceso con Peor Ajuste.");
                     }
+                    liberarRecursos(proceso.getListaRecursos());
                 }
-
+                
                 proceso.setTiempoRestante(0);
 //                System.out.println(proceso.getIdProceso() + " ha terminado.");
-
 
             }
             JOptionPane.showMessageDialog(null, "Simulación completada.");
             return null;
         }
-
+        
         @Override
         protected void process(List<Proceso> chunks) {
             for (Proceso proceso : chunks) {
@@ -198,13 +223,13 @@ public class Prioridad {
             }
         }
     }
-
+    
     private void correrTimempo(Proceso proceso) {
         if (tiempoActual < proceso.getTiempoLlegada()) {
             tiempoActual = proceso.getTiempoLlegada();
         }
     }
-
+    
     public int cpu(int rafaga, Proceso proceso) {
         try {
             Thread.sleep(2000);
@@ -215,50 +240,50 @@ public class Prioridad {
         proceso.setTiempoRestante(rafaga);
         return rafaga;
     }
-
+    
     public void cronometrarProceso(Proceso proceso) {
         proceso.setTiempoInicio(tiempoActual);
         proceso.setTiempoFinalizacion(tiempoActual + proceso.getTiempoRafaga());
         tiempoActual += proceso.getTiempoRafaga();
-
+        
     }
-
+    
     public ArrayList<Integer> obtenerFuturasReferencias(Proceso proceso) {
         ArrayList<Integer> futurasReferencias = new ArrayList<>();
-
+        
         for (Pagina pagina : proceso.getTablaPaginas().getPaginas()) {
             futurasReferencias.add(pagina.getIdPagina());
         }
-
+        
         return futurasReferencias;
     }
-
+    
     private void actualizarGrafica(Proceso proceso) {
         SwingUtilities.invokeLater(() -> {
             ventana.actualizarBarraDeProgreso(proceso);
         });
     }
-
+    
     public int getTiempoActual() {
         return tiempoActual;
     }
-
+    
     public void setTiempoActual(int tiempoActual) {
         this.tiempoActual = tiempoActual;
     }
-
+    
     public boolean isPaginacionXsegmentacion() {
         return paginacionXsegmentacion;
     }
-
+    
     public void setPaginacionXsegmentacion(boolean paginacionXsegmentacion) {
         this.paginacionXsegmentacion = paginacionXsegmentacion;
     }
-
+    
     public int getIdalgoritmosRemplazo() {
         return idalgoritmosRemplazo;
     }
-
+    
     public void setIdalgoritmosRemplazo(int idalgoritmosRemplazo) {
         this.idalgoritmosRemplazo = idalgoritmosRemplazo;
     }
